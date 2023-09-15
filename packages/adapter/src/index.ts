02861cdda8745719b1daef5b1336d9d29c1a37eb
jill64 <intents.turrets0h@icloud.com>
2023-09-15 23:51:23 +0900

    add lib index

---
+++ packages/adapter/src/index.ts
@@ -0,0 +1,55 @@
+import type { Adapter } from '@sveltejs/kit'
+import { match, P } from 'ts-pattern'
+import { edgeBundled } from './arch/edgeBundled.js'
+import { lambdaMono } from './arch/lambdaMono.js'
+import { lambdaS3 } from './arch/lambdaS3.js'
+import { deploy } from './deploy/index.js'
+import type { AdapterOptions } from './types/AdapterOptions.js'
+
+const name = 'adapter-aws'
+
+const adapter = (options?: AdapterOptions) => {
+  const { out = 'build', architecture, deploy: deployStep } = options ?? {}
+
+  return {
+    name,
+    adapt: async (builder) => {
+      builder.rimraf(out)
+
+      const tmp = builder.getBuildDirectory(name)
+      builder.rimraf(tmp)
+
+      await (
+        match(architecture)
+          .with('lambda-s3', () => lambdaS3)
+          .with('lambda-mono', () => lambdaMono)
+          .with('edge-bundled', () => edgeBundled)
+          .with('edge-unbundled', () => edgeBundled)
+          .with(P.nullish, () => {
+            builder.log.minor(
+              `Option 'architecture' is not defined. Use the default value 'lambda-s3'.`
+            )
+            return null
+          })
+          .otherwise(() => {
+            builder.log.minor(
+              `Option 'architecture' is invalid. Use the default value 'lambda-s3'.`
+            )
+            return null
+          }) ?? lambdaS3
+      )({ builder, options, tmp })
+
+      if (deployStep) {
+        await deploy(out)
+      }
+
+      if (deployStep === undefined) {
+        builder.log.minor(
+          `Option 'deploy' is not defined. Deploy step is skipped.`
+        )
+      }
+    }
+  } satisfies Adapter
+}
+
+export default adapter
