ed83bc648a270c592584f51f7eedb98aa5f1e15c
jill64 <intents.turrets0h@icloud.com>
2023-09-17 01:26:25 +0900

    ref: lambda-mono build

--- packages/adapter/src/arch/lambda-mono.ts
+++ packages/adapter/src/arch/lambda-mono.ts
@@ -1,13 +1,10 @@
 import { unfurl } from '@jill64/unfurl'
 import { build } from 'esbuild'
-import { readFile, writeFile } from 'fs/promises'
+import { writeFile } from 'fs/promises'
 import path from 'path'
-import { fileURLToPath } from 'url'
-import { Context } from '../../types/Context.js'
-import { listFiles } from '../../util/listFiles.js'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import { Context } from '../types/Context.js'
+import { copy } from '../utils/copy.js'
+import { listFiles } from '../utils/listFiles.js'
 
 export const lambdaMono = async ({ builder, options, tmp, out }: Context) => {
   const assets = path.join(out, 'assets')
@@ -16,12 +13,9 @@ export const lambdaMono = async ({ builder, options, tmp, out }: Context) => {
   builder.writePrerendered(assets)
   builder.writeServer(tmp)
 
-  const { list, serverSource } = await unfurl(
+  const { list } = await unfurl(
     {
-      list: listFiles(assets),
-      serverSource: readFile(path.join(__dirname, 'mock/server.ts')).then(
-        (res) => res.toString()
-      )
+      list: listFiles(assets)
     },
     writeFile(
       path.join(tmp, 'manifest.js'),
@@ -34,20 +28,29 @@ export const lambdaMono = async ({ builder, options, tmp, out }: Context) => {
     )
   )
 
+  const base = builder.config.kit.paths.base
+
   const staticAssetsPaths = list
     .map((file) => file.replace(assets, ''))
     .filter((file) => !file.startsWith('/_app/'))
+    .map((file) => path.join(base, file))
 
-  const convertedServerSource = serverSource.replace(
-    '[] /* $$__STATIC_ASSETS_PATHS__$$ */',
-    JSON.stringify(staticAssetsPaths)
-  )
+  // Embed values
+  const params = path.join('external', 'params')
+  const staticAssetsPath = path.join(params, 'staticAssetsPaths.ts')
+  const basePath = path.join(params, 'base.ts')
+
+  await Promise.all([
+    copy(`../embed/${staticAssetsPath}`, path.join(tmp, staticAssetsPath), {
+      '[] /* $$__STATIC_ASSETS_PATHS__$$ */': JSON.stringify(staticAssetsPaths)
+    }),
+    copy(`../embed/${basePath}`, path.join(tmp, basePath), {
+      "'' /* $$__BASE_PATH__$$ */": `'${base}'`
+    })
+  ])
 
   const serverEntryPoint = path.join(tmp, 'server.ts')
-  await writeFile(serverEntryPoint, convertedServerSource)
-
-  const outfile = path.join(out, 'server.js')
-  const shimsSource = path.join(__dirname, 'shims.ts')
+  builder.copy('../embed/arch/lambda-mono.ts', serverEntryPoint)
 
   await build({
     format: 'cjs',
@@ -56,8 +59,8 @@ export const lambdaMono = async ({ builder, options, tmp, out }: Context) => {
     external: ['node:*', '@aws-sdk/*'],
     ...options?.esbuild,
     entryPoints: [serverEntryPoint],
-    outfile,
+    outfile: path.join(out, 'server.js'),
     platform: 'node',
-    inject: [shimsSource]
+    inject: ['../embed/shims.ts']
   })
 }
