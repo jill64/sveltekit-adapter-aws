dbe2762f7207ad71e8237cd87a5a845580ebe878
jill64 <intents.turrets0h@icloud.com>
2023-09-17 00:26:24 +0900

    rename: cdk stacks

--- cdk/arch/edge-unbundled.ts
+++ cdk/arch/edge-unbundled.ts
@@ -2,7 +2,7 @@
 // import * as lambda from 'aws-cdk-lib/aws-lambda'
 // import { Construct } from 'constructs'
 
-// export class EdgeUnbundled extends Stack {
+// export class CDKStack extends Stack {
 //   constructor(scope: Construct, id: string, props?: StackProps) {
 //     super(scope, id, props)
 
--- cdk/arch/lambda-s3.ts
+++ cdk/arch/lambda-s3.ts
@@ -2,7 +2,7 @@
 // import * as lambda from 'aws-cdk-lib/aws-lambda'
 // import { Construct } from 'constructs'
 
-// export class LambdaS3 extends Stack {
+// export class CDKStack extends Stack {
 //   constructor(scope: Construct, id: string, props?: StackProps) {
 //     super(scope, id, props)
 
