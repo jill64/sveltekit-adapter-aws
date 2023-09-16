b580341de17ca5f0c880edad3b6a5f5b1dfb3c1d
jill64 <intents.turrets0h@icloud.com>
2023-09-17 00:02:33 +0900

    ref: arch server placeholder

---
+++ packages/adapter/src/arch/lambda-s3/cdk-stack.ts
@@ -0,0 +1,15 @@
+// import { Stack, StackProps } from 'aws-cdk-lib'
+// import * as lambda from 'aws-cdk-lib/aws-lambda'
+// import { Construct } from 'constructs'
+
+// export class LambdaS3 extends Stack {
+//   constructor(scope: Construct, id: string, props?: StackProps) {
+//     super(scope, id, props)
+
+//     new lambda.Function(this, 'HelloHandler', {
+//       runtime: lambda.Runtime.NODEJS_18_X,
+//       code: lambda.Code.fromAsset('lambda'),
+//       handler: 'hello.handler'
+//     })
+//   }
+// }
