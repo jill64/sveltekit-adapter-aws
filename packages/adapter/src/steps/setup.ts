1dd69dfcf4b2fac8a553a462fae57a9c1134943e
jill64 <intents.turrets0h@icloud.com>
2023-09-17 11:38:16 +0900

    fix: setup path

--- packages/adapter/src/steps/setup.ts
+++ packages/adapter/src/steps/setup.ts
@@ -1,10 +1,25 @@
 import path from 'path'
+import { fileURLToPath } from 'url'
 import type { Context } from '../types/Context.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export const setup = ({ builder, tmp, out }: Context) => {
   builder.log.minor('Setup...')
 
-  builder.copy('../embed/external', path.join(tmp, 'external'))
-  builder.copy('../cdk/cdk.json', path.join(out, 'cdk.json'))
-  builder.copy('../cdk/mock/synth.ts', path.join(out, 'bin', 'synth.ts'))
+  builder.copy(
+    path.resolve(__dirname, '../../embed/external'),
+    path.join(tmp, 'external')
+  )
+
+  builder.copy(
+    path.resolve(__dirname, '../../cdk/cdk.json'),
+    path.join(out, 'cdk.json')
+  )
+
+  builder.copy(
+    path.resolve(__dirname, '../../cdk/mock/synth.ts'),
+    path.join(out, 'bin', 'synth.ts')
+  )
 }
