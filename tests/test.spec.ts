6e7258770df918e126e7736c0a038a66cc6b7ebe
jill64 <intents.turrets0h@icloud.com>
2023-09-17 21:43:22 +0900

    add e2e

---
+++ tests/test.spec.ts
@@ -0,0 +1,24 @@
+import { expect, test } from '@playwright/test'
+import 'dotenv/config'
+
+test('Top', async ({ page }) => {
+  await page.goto('/')
+  await expect(
+    page.getByRole('heading', { name: '@jill64/sveltekit-adapter-aws' })
+  ).toBeVisible()
+})
+
+test('SPA', async ({ page }) => {
+  await page.goto('/csr')
+  await expect(page.getByRole('heading', { name: 'CSR' })).toBeVisible()
+})
+
+test('CSR', async ({ page }) => {
+  await page.goto('/csr')
+  await expect(page.getByRole('heading', { name: 'CSR' })).toBeVisible()
+})
+
+test('SSG', async ({ page }) => {
+  await page.goto('/ssg')
+  await expect(page.getByRole('heading', { name: 'SSG' })).toBeVisible()
+})
