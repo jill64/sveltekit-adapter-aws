49d5f4dc5d57580331cc55aaa42db82dcce9a14d
jill64 <intents.turrets0h@icloud.com>
2023-09-17 12:47:44 +0900

    add cdk skeleton package.json

--- packages/adapter/src/steps/setup.ts
+++ packages/adapter/src/steps/setup.ts
@@ -6,8 +6,11 @@ export const setup = ({ builder, tmp, options, out }: Context) => {
   builder.log.minor('Setup...')
 
   builder.copy(path.resolve(root, 'embed/external'), path.join(tmp, 'external'))
-
   builder.copy(path.resolve(root, 'cdk/cdk.json'), path.join(out, 'cdk.json'))
+  builder.copy(
+    path.resolve(root, 'cdk/package.json'),
+    path.join(out, 'package.json')
+  )
 
   builder.copy(
     path.resolve(root, 'cdk/mock/synth.ts'),
