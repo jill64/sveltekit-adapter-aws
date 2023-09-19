0fd1bc0d73d48001e5e50b927cca52a3c7f34b41
jill64 <intents.turrets0h@icloud.com>
2023-09-19 18:34:30 +0900

    add cdk-stack

--- cdk/arch/edge-bundled.ts
+++ cdk/arch/edge-bundled.ts
@@ -1,15 +1,76 @@
-// import { Stack, StackProps } from 'aws-cdk-lib'
-// import * as lambda from 'aws-cdk-lib/aws-lambda'
-// import { Construct } from 'constructs'
+import {
+  CfnOutput,
+  Duration,
+  Stack,
+  StackProps,
+  aws_certificatemanager,
+  aws_cloudfront,
+  aws_cloudfront_origins,
+  aws_lambda,
+  aws_s3,
+  aws_s3_deployment
+} from 'aws-cdk-lib'
+import { Construct } from 'constructs'
 
-// export class CDKStack extends Stack {
-//   constructor(scope: Construct, id: string, props?: StackProps) {
-//     super(scope, id, props)
+export class CDKStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
 
-//     new lambda.Function(this, 'HelloHandler', {
-//       runtime: lambda.Runtime.NODEJS_18_X,
-//       code: lambda.Code.fromAsset('lambda'),
-//       handler: 'hello.handler'
-//     })
-//   }
-// }
+    const domainName = '__DOMAIN_NAME__'
+    const certificateArn = '__CERTIFICATE_ARN__'
+
+    const edge = new aws_cloudfront.experimental.EdgeFunction(this, 'Edge', {
+      code: aws_lambda.Code.fromAsset('edge'),
+      handler: 'server.handler',
+      runtime: aws_lambda.Runtime.NODEJS_18_X,
+      timeout: Duration.seconds(5)
+    })
+
+    const s3 = new aws_s3.Bucket(this, 'Bucket')
+
+    const cdn = new aws_cloudfront.Distribution(this, 'CloudFront', {
+      domainNames: domainName ? [domainName] : undefined,
+      certificate: certificateArn
+        ? aws_certificatemanager.Certificate.fromCertificateArn(
+            this,
+            'CertificateManagerCertificate',
+            certificateArn
+          )
+        : undefined,
+      defaultBehavior: {
+        allowedMethods: aws_cloudfront.AllowedMethods.ALLOW_GET_HEAD,
+        cachePolicy: aws_cloudfront.CachePolicy.CACHING_OPTIMIZED,
+        viewerProtocolPolicy:
+          aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        originRequestPolicy: aws_cloudfront.OriginRequestPolicy.ALL_VIEWER,
+        origin: new aws_cloudfront_origins.S3Origin(s3),
+        edgeLambdas: [
+          {
+            functionVersion: edge,
+            eventType: aws_cloudfront.LambdaEdgeEventType.VIEWER_REQUEST,
+            includeBody: true
+          }
+        ]
+      },
+      httpVersion: aws_cloudfront.HttpVersion.HTTP2_AND_3
+    })
+
+    new aws_s3_deployment.BucketDeployment(this, 'S3Deploy', {
+      sources: [aws_s3_deployment.Source.asset('s3')],
+      destinationBucket: s3,
+      distribution: cdn
+    })
+
+    if (domainName) {
+      new CfnOutput(this, 'Deployed URL', {
+        description: 'Deployed URL',
+        value: `https://${domainName}`
+      })
+    }
+
+    new CfnOutput(this, 'CloudFront URL', {
+      description: 'CloudFront URL',
+      value: `https://${cdn.distributionDomainName}`
+    })
+  }
+}
