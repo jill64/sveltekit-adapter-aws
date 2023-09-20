7d0432f86667d7a26664de84c591bf129f7e5e03
jill64 <intents.turrets0h@icloud.com>
2023-09-20 21:13:38 +0900

    add server source

---
+++ embed/arch/edge-unbundled/edge.ts
@@ -0,0 +1,38 @@
+import { staticAssetsPaths } from '../../external/params/staticAssetsPaths.js'
+import type { OriginRequestHandler } from '../../external/types/edge/OriginRequestHandler.js'
+
+export const handler: OriginRequestHandler = (event) => {
+  const request = event.Records[0].cf.request
+  const { uri, method } = request
+
+  if (method === 'GET' || method === 'HEAD') {
+    const s3Domain = request.origin.custom.customHeaders['S3-Domain']
+
+    // Handling static asset requests
+    if (staticAssetsPaths.has(uri)) {
+      request.origin.custom.domainName = s3Domain
+      request.headers['host'] = [{ key: 'host', value: s3Domain }]
+
+      return request
+    }
+
+    // SSG requests fallback
+    if (uri.endsWith('/') && staticAssetsPaths.has(`${uri}index.html`)) {
+      request.origin.custom.domainName = s3Domain
+      request.headers['host'] = [{ key: 'host', value: s3Domain }]
+      request.uri = `${uri}index.html`
+
+      return request
+    }
+
+    if (staticAssetsPaths.has(`${uri}.html`)) {
+      request.origin.custom.domainName = s3Domain
+      request.headers['host'] = [{ key: 'host', value: s3Domain }]
+      request.uri = `${uri}.html`
+
+      return request
+    }
+  }
+
+  return request
+}
