5fabc4691c2ccbfa40ed911f54e0eed88a47026a
jill64 <intents.turrets0h@icloud.com>
2023-09-20 22:41:34 +0900

    fix: edge handler types

--- embed/external/types/edge/OriginRequestHandler.ts
+++ embed/external/types/edge/OriginRequestHandler.ts
@@ -1,7 +1,6 @@
-import { MaybePromise } from '@sveltejs/kit'
 import type { EdgeRequest } from './EdgeRequest.js'
 import { OriginRequestEvent } from './OriginRequestEvent.js'
 
 export type OriginRequestHandler = (
   event: OriginRequestEvent
-) => MaybePromise<EdgeRequest>
+) => Promise<EdgeRequest>
--- embed/external/types/edge/ViewerRequestHandler.ts
+++ embed/external/types/edge/ViewerRequestHandler.ts
@@ -1,8 +1,7 @@
-import { MaybePromise } from '@sveltejs/kit'
 import type { EdgeRequest } from './EdgeRequest.js'
 import type { EdgeResponse } from './EdgeResponse.js'
 import { ViewerRequestEvent } from './ViewerRequestEvent.js'
 
 export type ViewerRequestHandler = (
   event: ViewerRequestEvent
-) => MaybePromise<EdgeResponse | EdgeRequest>
+) => Promise<EdgeResponse | EdgeRequest>
