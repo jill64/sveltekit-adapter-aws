a0f50b45f778e5ebcc3c8cb24a2aea35919298c2
jill64 <intents.turrets0h@icloud.com>
2023-09-20 15:21:58 +0900

    apply to edge-bundled

--- packages/adapter/src/arch/edge-bundled.ts
+++ packages/adapter/src/arch/edge-bundled.ts
@@ -79,11 +79,16 @@ export const edgeBundled = async ({ builder, options, tmp, out }: Context) => {
     }
   )
 
-  // Copy .env file
-  builder.copy(
-    path.resolve(tmp, '../../', '.env'),
-    path.join(out, 'edge', '.env')
-  )
+  // Make .env file
+  if (options?.env) {
+    await writeFile(
+      path.join(out, 'edge', '.env'),
+      Object.entries(options.env).reduce(
+        (acc, [key, value]) => `${acc}${key}=${value}\n`,
+        ''
+      )
+    )
+  }
 
   const edgeEntryPoint = path.join(tmp, 'edge', 'index.ts')
   builder.copy(
