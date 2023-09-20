1634d3ba0fafbb6671858088a4e03bd4d6ec501b
jill64 <intents.turrets0h@icloud.com>
2023-09-20 21:28:54 +0900

    add adapter source

--- packages/adapter/src/arch/edge-unbundled.ts
+++ packages/adapter/src/arch/edge-unbundled.ts
@@ -1,52 +1,142 @@
-// import path from 'path'
-// import type { PropagationArgs } from '../../types/PropagationArgs.js'
+import { unfurl } from '@jill64/unfurl'
+import { build } from 'esbuild'
+import { writeFile } from 'fs/promises'
+import { nanoid } from 'nanoid'
+import path from 'path'
+import { Context } from '../types/Context.js'
+import { copy } from '../utils/copy.js'
+import { listFiles } from '../utils/listFiles.js'
+import { root } from '../utils/root.js'
 
-// export const edgeUnbundled = async (args: PropagationArgs) => {
-//   const { builder } = args
+export const edgeUnbundled = async ({
+  builder,
+  options,
+  tmp,
+  out
+}: Context) => {
+  const {
+    appDir,
+    paths: { base }
+  } = builder.config.kit
 
-//   const clientDir = path.join(tmp, 'client')
-//   builder.writeClient(clientDir)
+  const s3Assets = path.join(out, 's3', base)
 
-//   const preRenderedDir = path.join(tmp, 'pre-rendered')
-//   builder.writePrerendered(preRenderedDir)
+  builder.writeClient(s3Assets)
+  builder.writePrerendered(s3Assets)
+  builder.writeServer(tmp)
 
-//   const list = await listFiles(clientDir)
+  const { list } = await unfurl(
+    {
+      list: listFiles(s3Assets)
+    },
+    writeFile(
+      path.join(tmp, 'manifest.js'),
+      `export const manifest = ${builder.generateManifest({
+        relativePath: './'
+      })};\n\n` +
+        `export const prerendered = new Set(${JSON.stringify(
+          builder.prerendered.paths
+        )});\n`
+    )
+  )
 
-//   await writeFile(
-//     path.join(tmp, 'manifest.js'),
-//     `export const manifest = ${builder.generateManifest({
-//       relativePath: './'
-//     })};\n\n`
-//   )
+  const staticAssetsPaths = list
+    .map((file) => file.replace(s3Assets, ''))
+    .filter((file) => !file.startsWith(`/${appDir}/`))
+    .map((file) => path.join(base, file))
 
-//   writeFile(
-//     path.join(tmp, 'manifest.js'),
-//     `export const manifest = ${builder.generateManifest({
-//       relativePath: './'
-//     })};\n\n` +
-//       `export const prerendered = new Set(${JSON.stringify(
-//         builder.prerendered.paths
-//       )});\n`
-//   )
+  const bridgeAuthToken = nanoid()
 
-//   const serverSource = readFileSync('./adapter/src/server.ts').toString()
-//   const convertedServerSource = serverSource.replace(
-//     '[] /* $$__STATIC_ASSETS_PATHS__$$ */',
-//     JSON.stringify(list)
-//   )
+  // Copy CDK Stack
+  await copy(
+    path.join(root, 'cdk/arch/edge-unbundled.ts'),
+    path.join(out, 'bin', 'cdk-stack.ts'),
+    {
+      '128 /* $$__MEMORY_SIZE__$$ */': (options?.memory ?? 128).toString(),
+      'false /* $$__ENABLE_CDN__$$ */': options?.cdn ? 'true' : 'false',
+      __APP_DIR__: appDir,
+      __BASE_PATH__: base,
+      __BRIDGE_AUTH_TOKEN__: bridgeAuthToken,
+      __DOMAIN_NAME__: options?.domain?.fqdn ?? '',
+      __CERTIFICATE_ARN__: options?.domain?.certificateArn ?? '',
+      '{} /* $$__ENVIRONMENT__$$ */': JSON.stringify(options?.env ?? {})
+    }
+  )
 
-//   const convertedServerSourcePath = `${tmp}/server.ts`
+  // Embed values
+  const params = path.join('external', 'params')
+  const staticAssetsPath = path.join(params, 'staticAssetsPaths.ts')
 
-//   writeFile(convertedServerSourcePath, convertedServerSource)
+  await copy(
+    path.join(root, 'embed', staticAssetsPath),
+    path.join(tmp, staticAssetsPath),
+    {
+      '[] /* $$__STATIC_ASSETS_PATHS__$$ */': JSON.stringify(staticAssetsPaths)
+    }
+  )
 
-//   build({
-//     entryPoints: [convertedServerSourcePath],
-//     outfile: `${out}/server.js`,
-//     inject: ['./adapter/src/shims.ts'],
-//     external: ['node:*', 'sharp', '@aws-sdk/*'],
-//     format: 'cjs',
-//     bundle: true,
-//     platform: 'node',
-//     minify: true
-//   })
-// }
+  const basePath = path.join(params, 'base.ts')
+  builder.copy(path.join(root, 'embed', basePath), path.join(tmp, basePath), {
+    replace: {
+      __BASE_PATH__: base
+    }
+  })
+
+  const appDirPath = path.join(params, 'appDir.ts')
+  builder.copy(
+    path.join(root, 'embed', appDirPath),
+    path.join(tmp, appDirPath),
+    {
+      replace: {
+        __APP_DIR__: appDir
+      }
+    }
+  )
+
+  const bridgeAuthTokenPath = path.join(params, 'bridgeAuthToken.ts')
+  builder.copy(
+    path.join(root, 'embed', bridgeAuthTokenPath),
+    path.join(tmp, bridgeAuthTokenPath),
+    {
+      replace: {
+        __BRIDGE_AUTH_TOKEN__: bridgeAuthToken
+      }
+    }
+  )
+
+  const serverEntryPoint = path.join(tmp, 'server', 'lambda', 'index.ts')
+  builder.copy(
+    path.join(root, 'embed/arch/edge-unbundled/server.ts'),
+    serverEntryPoint
+  )
+
+  await build({
+    format: 'cjs',
+    bundle: true,
+    minify: true,
+    external: ['node:*', '@aws-sdk/*'],
+    ...options?.esbuild,
+    entryPoints: [serverEntryPoint],
+    outfile: path.join(out, 'lambda', 'server.js'),
+    platform: 'node',
+    inject: [path.join(root, 'embed', 'shims.ts')]
+  })
+
+  const edgeEntryPoint = path.join(tmp, 'server', 'edge', 'index.ts')
+  builder.copy(
+    path.join(root, 'embed/arch/edge-unbundled/edge.ts'),
+    edgeEntryPoint
+  )
+
+  await build({
+    format: 'cjs',
+    bundle: true,
+    minify: true,
+    external: ['node:*'],
+    ...options?.esbuild,
+    entryPoints: [edgeEntryPoint],
+    outfile: path.join(out, 'edge', 'server.js'),
+    platform: 'node',
+    inject: [path.join(root, 'embed', 'shims.ts')]
+  })
+}
