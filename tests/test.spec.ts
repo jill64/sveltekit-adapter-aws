149e4996b6fba545881dbcd5408175cc215a139a
jill64 <intents.turrets0h@icloud.com>
2023-09-21 18:19:26 +0900

    ref: test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test'
 
-test('Top', async ({ page }) => {
+test('SSR', async ({ page }) => {
   await page.goto('/')
   await expect(
     page.getByRole('heading', { name: '@jill64/sveltekit-adapter-aws' })
@@ -9,15 +9,24 @@ test('Top', async ({ page }) => {
 
 test('SPA', async ({ page }) => {
   await page.goto('/csr')
+  await page.goto('/')
   await expect(page.getByRole('heading', { name: 'CSR' })).toBeVisible()
 })
 
 test('CSR', async ({ page }) => {
+  await page.goto('/')
   await page.goto('/csr')
   await expect(page.getByRole('heading', { name: 'CSR' })).toBeVisible()
 })
 
 test('SSG', async ({ page }) => {
+  await page.goto('/ssg')
+  await page.goto('/')
+  await expect(page.getByRole('heading', { name: 'SSG' })).toBeVisible()
+})
+
+test('SSG Routing', async ({ page }) => {
+  await page.goto('/')
   await page.goto('/ssg')
   await expect(page.getByRole('heading', { name: 'SSG' })).toBeVisible()
 })
