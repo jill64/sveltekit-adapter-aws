4195151c1cd14843493539aa912ec00e5d1af80c
jill64 <intents.turrets0h@icloud.com>
2023-09-21 17:04:06 +0900

    omit: sentry integration

--- packages/site/src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import { PUBLIC_SENTRY_DSN } from '$env/static/public'
-import { serverInit } from '@jill64/sentry-sveltekit-cloudflare'
-
-const { onHandle, onError } = serverInit(PUBLIC_SENTRY_DSN)
-
-export const handle = onHandle()
-export const handleError = onError()
