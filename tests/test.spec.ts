2905bc0723950aa41408ee95347a0314cfb3b40d
jill64 <intents.turrets0h@icloud.com>
2023-09-21 20:30:18 +0900

    fix: test pattern

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -9,24 +9,40 @@ test('SSR', async ({ page }) => {
 
 test('SPA', async ({ page }) => {
   await page.goto('/csr')
-  await page.goto('/')
   await expect(page.getByRole('heading', { name: 'CSR' })).toBeVisible()
+
+  await page.goto('/')
+  await expect(
+    page.getByRole('heading', { name: '@jill64/sveltekit-adapter-aws' })
+  ).toBeVisible()
 })
 
 test('CSR', async ({ page }) => {
   await page.goto('/')
+  await expect(
+    page.getByRole('heading', { name: '@jill64/sveltekit-adapter-aws' })
+  ).toBeVisible()
+
   await page.goto('/csr')
   await expect(page.getByRole('heading', { name: 'CSR' })).toBeVisible()
 })
 
 test('SSG', async ({ page }) => {
   await page.goto('/ssg')
-  await page.goto('/')
   await expect(page.getByRole('heading', { name: 'SSG' })).toBeVisible()
+
+  await page.goto('/')
+  await expect(
+    page.getByRole('heading', { name: '@jill64/sveltekit-adapter-aws' })
+  ).toBeVisible()
 })
 
 test('SSG Routing', async ({ page }) => {
   await page.goto('/')
+  await expect(
+    page.getByRole('heading', { name: '@jill64/sveltekit-adapter-aws' })
+  ).toBeVisible()
+
   await page.goto('/ssg')
   await expect(page.getByRole('heading', { name: 'SSG' })).toBeVisible()
 })
