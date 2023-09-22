ed0a043af02bac4d630651ab6e72d3603a0dba8d
jill64 <intents.turrets0h@icloud.com>
2023-09-23 01:04:37 +0900

    omit: unuse types

--- embed/external/types/edge/ViewerRequestHandler.ts
+++
@@ -1,7 +0,0 @@
-import type { EdgeRequest } from './EdgeRequest.js'
-import type { EdgeResponse } from './EdgeResponse.js'
-import { ViewerRequestEvent } from './ViewerRequestEvent.js'
-
-export type ViewerRequestHandler = (
-  event: ViewerRequestEvent
-) => Promise<EdgeResponse | EdgeRequest>
