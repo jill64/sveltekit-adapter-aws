c066ae3fed0633e8b9f766489ba38825cef4ba04
jill64 <intents.turrets0h@icloud.com>
2023-09-27 12:14:52 +0900

    format

--- embed/arch/lambda-s3.ts
+++ embed/arch/lambda-s3.ts
@@ -7,53 +7,55 @@ import { verdictStaticAssets } from '../external/utils/verdictStaticAssets.js'
 
 declare const awslambda: AwsLambda
 
-export const handler = awslambda.streamifyResponse(async (request, responseStream) => {
-  const {
-    requestContext: {
-      http: { method, sourceIp },
-      domainName
-    },
-    headers,
-    rawPath: pathname,
-    rawQueryString,
-    body,
-    isBase64Encoded
-  } = request
+export const handler = awslambda.streamifyResponse(
+  async (request, responseStream) => {
+    const {
+      requestContext: {
+        http: { method, sourceIp },
+        domainName
+      },
+      headers,
+      rawPath: pathname,
+      rawQueryString,
+      body,
+      isBase64Encoded
+    } = request
 
-  if (isDirectAccess({ request, responseStream, awslambda })) {
-    return
-  }
+    if (isDirectAccess({ request, responseStream, awslambda })) {
+      return
+    }
 
-  const assetPath = verdictStaticAssets({
-    method,
-    pathname
-  })
+    const assetPath = verdictStaticAssets({
+      method,
+      pathname
+    })
 
-  if (assetPath) {
-    return streamFile({
-      assetPath,
+    if (assetPath) {
+      return streamFile({
+        assetPath,
+        responseStream,
+        awslambda
+      })
+    }
+
+    const response = await respond({
+      domain: domainName,
+      pathname,
+      queryString: rawQueryString,
+      method,
+      body,
+      headers,
+      sourceIp,
+      isBase64Encoded
+    })
+
+    // TODO: If the response header is too long, a 502 error will occur on Gateway, so delete it.
+    response.headers.delete('link')
+
+    return runStream({
+      response,
       responseStream,
       awslambda
     })
   }
-
-  const response = await respond({
-    domain: domainName,
-    pathname,
-    queryString: rawQueryString,
-    method,
-    body,
-    headers,
-    sourceIp,
-    isBase64Encoded
-  })
-
-  // TODO: If the response header is too long, a 502 error will occur on Gateway, so delete it.
-  response.headers.delete('link')
-
-  return runStream({
-    response,
-    responseStream,
-    awslambda
-  })
-})
+)
