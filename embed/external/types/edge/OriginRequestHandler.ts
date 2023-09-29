4d61465be42433f4fe4a1c83f7946e4d722bd255
jill64 <intents.turrets0h@icloud.com>
2023-09-30 01:55:08 +0900

    omit: types

--- embed/external/types/edge/OriginRequestHandler.ts
+++
@@ -1,7 +0,0 @@
-import type { EdgeRequest } from './EdgeRequest.js'
-import { EdgeResponse } from './EdgeResponse.js'
-import { OriginRequestEvent } from './OriginRequestEvent.js'
-
-export type OriginRequestHandler = (
-  event: OriginRequestEvent
-) => Promise<EdgeRequest | EdgeResponse>
