3dd599d1e2e6ae11ec4e4bfdcdcd7e6add01acb8
jill64 <143883742+jill64@users.noreply.github.com>
2023-10-24 09:03:18 +0900

    chore: add playwright config , test

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,12 +2,34 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
   use: {
-    baseURL: `https://${process.env.ADAPTER_FQDN}`
+    baseURL: `https://${process.env.ADAPTER_FQDN}`,
   },
-  testDir: 'tests',
-  testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+  testDir: "tests",
+  retries: process.env.CI ? 2 : 0,
   fullyParallel: true,
-  workers: '100%'
-}
+  workers: "100%",
+  projects: [
+    {
+      name: "chromium",
+      use: devices["Desktop Chrome"],
+    },
+    {
+      name: "firefox",
+      use: devices["Desktop Firefox"],
+    },
+    {
+      name: "webkit",
+      use: devices["Desktop Safari"],
+    },
+    {
+      name: "Mobile Chrome",
+      use: devices["Pixel 5"],
+    },
+    {
+      name: "Mobile Safari",
+      use: devices["iPhone 12"],
+    },
+  ],
+};
 
 export default config
