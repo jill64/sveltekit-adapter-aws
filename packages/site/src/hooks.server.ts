21cd9bde1dca4a44a1e0bced5364a926b5063789
jill64 <intents.turrets0h@icloud.com>
2023-11-18 19:05:13 +0900

    add error logging

---
+++ packages/site/src/hooks.server.ts
@@ -0,0 +1,14 @@
+import { serverInit } from '@jill64/sentry-sveltekit-edge'
+
+const { onHandle, onError } = serverInit(
+  'https://6a55c18dec92e5a5c7a2c3090df75f00@o4505814639312896.ingest.sentry.io/4506161269047296'
+)
+
+export const handle = onHandle(({ resolve, event }) => {
+  console.log('handle', event)
+  return resolve(event)
+})
+
+export const handleError = onError((e) => {
+  console.log('error', e)
+})
